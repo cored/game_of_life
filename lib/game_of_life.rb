9761e53064fe5e69395f20af382ab0a9dc23f611
Rafael George <george.rafael@gmail.com>
2022-06-27 16:28:17 -0600

    feat: add GameOfLife module
    - handles generationos fo different worlds

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,23 +1,15 @@
+require_relative 'worlds'
+
 module GameOfLife
   extend self
 
-  module Worlds
-    Replace =->(world) do
-      world
-    end
-  end
+  def call(generations, replace_world = Worlds::Replace)
+    world = Worlds::World.build
 
-  def call(generations,
-           time_limit,
-           generate_world, replace_world = Worlds::Replace, display_world)
-    world = generate_world.call
-
-    1.upto(generations) do |_|
-      display_world.(world)
-      world = replace_world.(world)
+    worlds = 1.upto(generations).map do |_|
+      replace_world.(world)
     end
 
-    display_world.(world)
   end
 
 end
